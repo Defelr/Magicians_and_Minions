21379e80bdacefeecbe8baa983e6e6e21a1c2c48
Carlson <carlsm4@win.rpi.edu>
2018-02-19 15:45:37 -0500

    grid added to the board and spawning of player1 is ready

---
+++ Magic and Minions/Assets/Spawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+    public GameObject Player1;
+	// Use this for initialization
+	void Start () {
+        Player1.transform.localScale = new Vector3(15F, 15F, 15F);
+        Instantiate(Player1, transform.position, transform.rotation);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
