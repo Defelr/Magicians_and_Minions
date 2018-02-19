476bfbf94fb397a358d0b7008998f221654f75d9
Carlson <carlsm4@win.rpi.edu>
2018-02-19 16:42:23 -0500

    Started working on the implemintation of movement

---
+++ Magic and Minions/Assets/MouseDetect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDetect : MonoBehaviour {
+    bool onClick = false;
+    public GameObject grid_B;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnMouseDown()
+    {
+        grid_B = GameObject.Find("Grid_Board");
+      /*  foreach(GameObject child in grid_B)
+        {
+            if()
+        }*/
+    }
+
+}
