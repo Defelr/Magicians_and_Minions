fd74a9939789d3de5b711ec2093f99810a177901
mdcpanama22 <mdcpanama22@gmail.com>
2018-03-07 01:45:47 -0500

    Finished movement and summoning mechanics

--- Magic and Minions/Assets/BlockChoice.cs
+++ Magic and Minions/Assets/BlockChoice.cs
@@ -2,50 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlockChoice : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class BlockChoice : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
     private void Update()
     {
     }
-    private void OnMouseDown () {
+    private void OnMouseDown()
+    {
         Renderer Re = null;
         Renderer R = GetComponent<Renderer>();
         Collider C = GetComponent<Collider>();
         if (R.enabled)
         {
-            if(DDOL.instance.option == "summon")
+            if (DDOL.instance.option == "summon")
             {
-                DDOL.instance.option = "NaN";
                 DDOL.instance.SummonPawn(transform);
-                foreach(GameObject c in DDOL.instance.spaces)
+                foreach (GameObject c in DDOL.instance.spaces)
                 {
                     Re = c.GetComponent<Renderer>();
                     Re.enabled = false;
                 }
                 DDOL.instance.spaces.Clear();
-                for (int i = 0; i < DDOL.instance.x; i++)
-                {
-                    for (int j = 0; j < DDOL.instance.x; j++)
-                    {
-                        if(Re == DDOL.instance.Coords[i][j].location.GetComponent<Renderer>())
-                        {
-                               // DDOL.instance.Coords[i][j].G = 
-                        }
-                    }
-                    Debug.Log("\n");
-                }
             }
-            else
+            if (DDOL.instance.option == "move")
             {
                 Debug.Log("You can move Here!");
                 DDOL.instance.MoveCharacter(transform);
             }
             C.isTrigger = true;
+            DDOL.instance.option = "NaN";
         }
     }
 }
--- Magic and Minions/Assets/MouseDetect.cs
+++ Magic and Minions/Assets/MouseDetect.cs
@@ -2,51 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseDetect : MonoBehaviour {
-    bool onClick = false;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
-    public void On_Clicked()
+public class MouseDetect : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
     {
-            
-        Debug.Log("Start");
-        Collider colp = GetComponent<Collider>();
-        GameObject grid_B = GameObject.Find("Grid_Board");
-        //if (!onClick)
-        //{
-            //onClick = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject grid_B = GameObject.Find("Grid_Board");
+            DDOL.instance.ClearSpaces();
             foreach (Transform child in grid_B.transform)
             {
                 Collider col = child.GetComponent<Collider>();
-                if (col.isTrigger /*&& !colp.isTrigger*/) //Uncomment so that people can only move once. When turn ends, all triggers should go back to false
+                Collider thiscol = this.GetComponent<Collider>();
+                if (col.bounds.Intersects(thiscol.bounds))
                 {
-
-                    DDOL.instance.Movement(child.gameObject, gameObject);
-                    foreach (GameObject c in DDOL.instance.spaces)
+                    for (int i = 0; i < DDOL.instance.x; i++)
                     {
-                        // Debug.Log(c.gameObject.name);
-                        Renderer R = c.GetComponent<Renderer>();
-                        R.enabled = true;
+                        for (int j = 0; j < DDOL.instance.x; j++)
+                        {
+                            Debug.Log("Mouse Down");
+                            Debug.Log(DDOL.instance.Coords[i][j].location.gameObject.name);
+                            Debug.Log(col.gameObject.name);
+                            if (DDOL.instance.Coords[i][j].location.gameObject.name == col.gameObject.name)
+                            {
+                                Debug.Log("IN ID ADDER");
+                                DDOL.instance.Coords[i][j] = DDOL.instance.SetObject(this.gameObject.GetInstanceID(), 1, this.gameObject, DDOL.instance.Coords[i][j].location);
+                                Debug.Log(this.gameObject.GetInstanceID());
+                                Debug.Log(DDOL.instance.Coords[i][j].ID);
+                                return;
+                            }
+                        }
                     }
-                    DDOL.instance.currentObject = gameObject;
-                    Debug.Log(name);
                 }
-                DDOL.instance.option = "move";
+
             }
-        //}
-        //else
-        //{
-            //if(DDOL.instance.currentObject == null)
-            //{ 
-                //onClick = false;
-            //}
-        //}
+        }
+    }
+    public void On_Clicked()
+    {
+        Debug.Log("Start");
+        List<GameObject> spaces = new List<GameObject>();
+        spaces = DDOL.instance.SpaceLocation(1, DDOL.instance.currentObject.GetInstanceID());
+        if (spaces.Count <= 0)
+        {
+            Debug.Log("Can't Move");
+        }
+        else
+        {
+            foreach (GameObject c in spaces)
+            {
+                Renderer R = c.GetComponent<Renderer>();
+                R.enabled = true;
+            }
+            DDOL.instance.option = "move";
+        }
     }
 }
