0922c16951f872c7183ee77330cdd844302c9954
Robert DeFelice <trombonedrummer@gmail.com>
2018-03-21 15:12:13 -0400

    Revert "Final merge changes"
    This reverts commit a22fa7db71d21a1c22d3dad7ff5343a8c7962495.

--- Magic and Minions/Assets/Switch_Canvas.cs
+++ Magic and Minions/Assets/Switch_Canvas.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class Switch_Canvas : MonoBehaviour {
 
-    public GameObject necroInterface;
-    public GameObject minionInterface;
+    public GameObject necroCanvas;
+    public GameObject minionCanvas;
     public GameObject wraithImage;
     public GameObject skelImage;
 
@@ -59,7 +59,6 @@ public class Switch_Canvas : MonoBehaviour {
                 }
                 if (hitInfo.transform.gameObject.tag == "Necro")
                 {
-<<<<<<< HEAD
                     foreach(Transform TPanel in MenuCanvasPanel.transform)
                     {
                         if(TPanel.tag != "Necro")
@@ -71,12 +70,6 @@ public class Switch_Canvas : MonoBehaviour {
                             TPanel.gameObject.SetActive(true);
                         }
                     }
-=======
-                    minionInterface.SetActive(false);
-                    necroInterface.SetActive (true);
-                    wraithImage.SetActive(false);
-                    skelImage.SetActive(false);
->>>>>>> dev_Rob
                     necroMana.text = DDOL.instance.Coords[temp_x][temp_y].D.MANA.ToString();
                     necroHP.text = DDOL.instance.Coords[temp_x][temp_y].D.HP.ToString();
 
@@ -84,7 +77,6 @@ public class Switch_Canvas : MonoBehaviour {
                 } 
                 if (hitInfo.transform.gameObject.tag == "Wraith")
                 {
-<<<<<<< HEAD
                     foreach (Transform TPanel in MenuCanvasPanel.transform)
                     {
                         if (TPanel.tag != "Wraith")
@@ -112,19 +104,6 @@ public class Switch_Canvas : MonoBehaviour {
                         }
                     }
                     MenuCanvasPanel.transform.Find("Minion_Canvas").gameObject.SetActive(true);
-=======
-                    necroInterface.SetActive(false);
-                    minionInterface.SetActive(true);
-                    skelImage.SetActive(false);
-                   wraithImage.SetActive(true);
-                }
-                else if (hitInfo.transform.gameObject.tag == "Skeleton" && DDOL.instance.currentObject.tag == "Skeleton")
-                {
-                    necroInterface.SetActive(false);
-                    minionInterface.SetActive(true);
-                    wraithImage.SetActive(false);
-                    skelImage.SetActive(true);
->>>>>>> dev_Rob
                 }
             }
        }
