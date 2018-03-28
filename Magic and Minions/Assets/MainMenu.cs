4700110630a9b7808b1d538e417fe91b146d9969
Robert DeFelice <trombonedrummer@gmail.com>
2018-03-28 12:45:41 -0400

    Added Selection Scene to multiplayer and Created Srpint 3 build
    Selection screen only shows necromancer

--- Magic and Minions/Assets/MainMenu.cs
+++ Magic and Minions/Assets/MainMenu.cs
@@ -13,7 +13,7 @@ public class MainMenu : MonoBehaviour {
 
     public void Multiplayer()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(3);
     }
 
     public void Quit()
