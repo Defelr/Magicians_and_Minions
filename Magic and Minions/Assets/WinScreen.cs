1a228eefbd039f4c7a25e4af88f61ef31960ae67
Robert DeFelice <trombonedrummer@gmail.com>
2018-04-09 22:07:30 -0400

    Added new Ingame Menu
    Added a new ingame menu, cleaned up scripts and prefabs, and organized my scripts.

---
+++ Magic and Minions/Assets/WinScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour {
+    public GameObject P1;
+    public GameObject P2;
+    public GameObject winPnl;
+
+    public void ResetWC()
+    {
+        HotseatWin.winVar = 0;
+        winPnl.SetActive(false);
+        SceneManager.LoadScene(2);
+        //P1.SetActive(false);
+        //P2.SetActive(false);
+    }
+}
